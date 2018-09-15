55f4ac69e509c4cdcb84be2615cf9b25a1d78d72
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2018-09-15 13:11:36 -0700

    Use BEM to style the project view

--- src/project-view/component.tsx
+++ src/project-view/component.tsx
@@ -82,50 +82,50 @@ export class ProjectView extends React.Component<ProjectViewProps, State>{
     const manifest = rigProject.manifest;
     return (
       <div className="project-view">
-        <div>
-          <label>
-            <div>Project Name</div>
-            <input className="half" type="text" name="name" value={manifest.name} onChange={this.onChange} />
+        <div className="project-view__section project-view__section--left">
+          <label className="project-view-property">
+            <div className="project-view-property__name">Project Name</div>
+            <input className="project-view-property__input project-view-property__input--half" type="text" name="name" value={manifest.name} onChange={this.onChange} />
           </label>
-          <label title="This is the path to your front-end files relative to the Project Folder.  If there is no Project Folder, ensure this path is absolute.">
-            <div>Front-end Files Location</div>
-            <input type="text" name="frontendFolderName" value={rigProject.frontendFolderName} onChange={this.onChange} />
-            <button title="" onClick={this.startFrontend}>Host with Rig</button>
-            <span>{this.state.frontendResult}</span>
+          <label className="project-view-property" title="This is the path to your front-end files relative to the Project Folder.  If there is no Project Folder, ensure this path is absolute.">
+            <div className="project-view-property__name">Front-end Files Location</div>
+            <input className="project-view-property__input" type="text" name="frontendFolderName" value={rigProject.frontendFolderName} onChange={this.onChange} />
+            <button className="project-view__button" title="" onClick={this.startFrontend}>Host with Rig</button>
+            <span className="project-view-property__result">{this.state.frontendResult}</span>
           </label>
-          <label title="This is the command used to run your back-end.  If there is a Project Folder, this command is run with that folder as its current directory.">
-            <div>Back-end Run Command</div>
-            <input type="text" name="backendCommand" value={rigProject.backendCommand} onChange={this.onChange} />
-            <button title="" onClick={this.startBackend}>Activate</button>
-            <span>{this.state.backendResult}</span>
+          <label className="project-view-property" title="This is the command used to run your back-end.  If there is a Project Folder, this command is run with that folder as its current directory.">
+            <div className="project-view-property__name">Back-end Run Command</div>
+            <input className="project-view-property__input" type="text" name="backendCommand" value={rigProject.backendCommand} onChange={this.onChange} />
+            <button className="project-view__button" title="" onClick={this.startBackend}>Activate</button>
+            <span className="project-view-property__result">{this.state.backendResult}</span>
           </label>
-          <label>
-            <div>Project Folder</div>
-            <input type="text" name="projectFolderPath" value={rigProject.projectFolderPath} onChange={this.onChange} />
+          <label className="project-view-property">
+            <div className="project-view-property__name">Project Folder</div>
+            <input className="project-view-property__input" type="text" name="projectFolderPath" value={rigProject.projectFolderPath} onChange={this.onChange} />
           </label>
-          <label>
-            <div>Extension Types</div>
-            <div>{this.getExtensionViews(rigProject)}</div>
+          <label className="project-view-property">
+            <div className="project-view-property__name">Extension Types</div>
+            <div className="project-view-property__value">{this.getExtensionViews(rigProject)}</div>
           </label>
         </div>
-        <div className="vertical-bar" />
-        <div>
-          <div>How to Run an Extension in the Rig</div>
+        <div className="project-view__vertical-bar" />
+        <div className="project-view__section project-view__section--right">
+          <div className="project-view__title">How to Run an Extension in the Rig</div>
           <ol>
-            <li>
-              <div>Host your front-end files.</div>
-              <div>You can host your front-end files with the Rig by entering the path to your HTML files in the "Front-end Files Location" text box and clicking the "Host with Rig" button.</div>
+            <li className="project-view__item">
+              <div className="project-view__item-text project-view__item-text--title">Host your front-end files.</div>
+              <div className="project-view__item-text">You can host your front-end files with the Rig by entering the path to your HTML files in the "Front-end Files Location" text box and clicking the "Host with Rig" button.</div>
             </li>
-            <li>
-              <div>If your extension has a back-end, run it locally.</div>
-              <div>You can run your back-end service from the Rig by entering the command to activate it in the "Back-end Run Command" text box and clicking the "Activate" button.</div>
+            <li className="project-view__item">
+              <div className="project-view__item-text project-view__item-text--title">If your extension has a back-end, run it locally.</div>
+              <div className="project-view__item-text">You can run your back-end service from the Rig by entering the command to activate it in the "Back-end Run Command" text box and clicking the "Activate" button.</div>
             </li>
-            <li>
-              <div>Go to Extension Views and add the Extension Types that match your extension.</div>
+            <li className="project-view__item">
+              <div className="project-view__item-text project-view__item-text--title">Go to Extension Views and add the Extension Types that match your extension.</div>
             </li>
           </ol>
-          <button>View Tutorial</button>
-          <button>Go to Documentation</button>
+          <button className="project-view__button project-view__button--first">View Tutorial</button>
+          <button className="project-view__button">Go to Documentation</button>
         </div>
       </div>
     );
